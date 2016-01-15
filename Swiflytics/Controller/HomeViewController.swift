e2852aa8cf7b44b56530e170614267ab54af00cd
Ankit Aggarwal <ankit.spd@gmail.com>
2016-01-15 21:35:37 +0530

    Add basic sectioned tableview for home

--- Swiflytics/Controller/HomeViewController.swift
+++ Swiflytics/Controller/HomeViewController.swift
@@ -8,40 +8,72 @@
 
 import UIKit
 
-final class HomeViewController: UIViewController, StoryboardInstantiable, UITableViewDataSource, UITableViewDelegate {
+final class HomeViewController: UIViewController, StoryboardInstantiable {
 
     typealias ViewController = UINavigationController
 
     static let storyboardID = "HomeViewControllerID"
 
-    var dataSource = [String]()
+    @IBOutlet var tableView: UITableView!
+    
+    var dataSource = [String: [String]]()
+    let headers = ["one", "two", "three"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Swiflytics"
-        dataSource.append("ok")
-        dataSource.append("ok")
-        dataSource.append("ok")
-        dataSource.append("ok")
-        dataSource.append("ok")
-        dataSource.append("ok")
+        
+        let headers = ["one", "two", "three"]
+        
+        for header in headers {
+            dataSource[header] = ["one", "two", "three"]
+        }
+        tableView.tableFooterView = UIView()
     }
+    
+    @IBAction func logoutPressed() {
+        GIDSignIn.sharedInstance().signOut()
+        UIView.transitionWithView(self.view.window!, duration: 0.5, options: UIViewAnimationOptions.TransitionFlipFromLeft, animations: { () -> Void in
+            self.view.window?.rootViewController = LoginViewController.instance(self.storyboard!)
+            }, completion: nil)
+    }
+    
+    @IBAction func refreshPressed() {
+        tableView.reloadData()
+    }
+}
 
+extension HomeViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 38
+    }
+    
+    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return headers[section]
+    }
+    
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return dataSource.count
+        return dataSource[headers[section]]!.count
     }
-
+    
     func tableView(tableView: UITableView, heightForRowAtIndexPath indexPath: NSIndexPath) -> CGFloat {
         return 68.0
     }
-
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(String(HomeTableViewCell), forIndexPath: indexPath) as! HomeTableViewCell
         
         return cell
     }
-
+    
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return headers.count
+    }
+}
+
+extension HomeViewController: UITableViewDelegate {
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
     }
 }
