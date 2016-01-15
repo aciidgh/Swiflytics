1b566e2c9a646576fe072fbcf817af855ed0f972
Ankit Aggarwal <ankit.aggarwal@housing.com>
2016-01-15 12:57:11 +0530

    Initial Commit

---
+++ Swiflytics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swiflytics
+//
+//  Created by Ankit Aggarwal on 15/01/16.
+//  Copyright © 2016 ankit.im. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
